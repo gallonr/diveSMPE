bcb920bdc1907f492c1d1bcd58bb2fd14fc8235d
gallonr <regis.gallon@lecnam.net>
2026-04-29 19:28:55 +0200

    sync: mise à jour docs depuis pwa

--- docs/sw.js
+++ docs/sw.js
@@ -6,7 +6,7 @@
 
 // ── Configuration ─────────────────────────────────────────────
 const DEBUG   = false;          // Passer à true pour les logs en développement
-const VERSION = 'v9';
+const VERSION = 'v10';
 
 const CACHE_STATIC  = `smpe-static-${VERSION}`;
 const CACHE_DYNAMIC = `smpe-dynamic-${VERSION}`;
