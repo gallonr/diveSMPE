00ef5f0d0e5a27a6fc501964e564fb37398a92c2
gallonr <regis.gallon@lecnam.net>
2026-05-08 12:32:59 +0200

    fix: mise à jour automatique SW sur iOS (v16)

--- pwa/sw.js
+++ pwa/sw.js
@@ -6,7 +6,7 @@
 
 // ── Configuration ─────────────────────────────────────────────
 const DEBUG   = false;          // Passer à true pour les logs en développement
-const VERSION = 'v15';
+const VERSION = 'v16';
 
 const CACHE_STATIC  = `smpe-static-${VERSION}`;
 const CACHE_DYNAMIC = `smpe-dynamic-${VERSION}`;
