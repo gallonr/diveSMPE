21e09027213f92a22802c8de7713f1cdd4142cde
gallonr <regis.gallon@lecnam.net>
2026-05-08 11:53:44 +0200

    chore: SW v14

--- pwa/sw.js
+++ pwa/sw.js
@@ -6,7 +6,7 @@
 
 // ── Configuration ─────────────────────────────────────────────
 const DEBUG   = false;          // Passer à true pour les logs en développement
-const VERSION = 'v13';
+const VERSION = 'v14';
 
 const CACHE_STATIC  = `smpe-static-${VERSION}`;
 const CACHE_DYNAMIC = `smpe-dynamic-${VERSION}`;
