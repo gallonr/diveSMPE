6e33f2cc0912ed0622543e28b10c86789a70ad7e
gallonr <regis.gallon@lecnam.net>
2026-05-13 23:19:30 +0200

    chore: SW v34

--- pwa/sw.js
+++ pwa/sw.js
@@ -6,7 +6,7 @@
 
 // ── Configuration ─────────────────────────────────────────────
 const DEBUG   = false;          // Passer à true pour les logs en développement
-const VERSION = 'v33';
+const VERSION = 'v34';
 
 const CACHE_STATIC  = `smpe-static-${VERSION}`;
 const CACHE_DYNAMIC = `smpe-dynamic-${VERSION}`;
