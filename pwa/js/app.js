4c44b6e983aa902b5896eaccd502529bbb8925b4
gallonr <regis.gallon@lecnam.net>
2026-08-13 17:28:31 +0200

    feat: intègre RetourExperience dans app.js, bump SW v35

--- pwa/js/app.js
+++ pwa/js/app.js
@@ -57,7 +57,10 @@ const App = (() => {
     // 11. Init module CGU
     if (typeof Cgu !== 'undefined') Cgu.init();
 
-    // 12. Tutoriel premier démarrage
+    // 12. Init module Retour d'expérience
+    if (typeof RetourExperience !== 'undefined') RetourExperience.init();
+
+    // 13. Tutoriel premier démarrage
     if (typeof Tutorial !== 'undefined') Tutorial.init();
 
     // 9. Événements UI
